a0d11f63760770b64266073046744a3c1f7b9ee2
DARK TEAM LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2026-04-14 11:04:30 +0600

    Add files via upload
    Signed-off-by: DARK TEAM LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>

---
+++ SPAM_v10.py
@@ -0,0 +1,15 @@
+# Fuxk You Kids || - LMNx9
+
+import os,sys,platform,time
+lmnxsukhi = platform.architecture()[0]
+try:
+    if lmnxsukhi == '64bit':import SPAM_v10
+    else:
+        os.system("clear")
+        os.system('xdg-open https://t.me/+6hZVa9jGrIY4ZDRl')
+        time.sleep(3)
+        sys.exit("\n </> Your Device Is Not Supported For Run This Tool..!")
+except Exception as xx:
+    os.system("clear")
+    sys.exit(f"\n </> Something Went Wrong..! Try Again\n <?> Error : {xx}")
+    
