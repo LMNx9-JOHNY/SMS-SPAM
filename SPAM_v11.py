12d2c24055fb5cdfcbdefbbf3880e071230bc291
DARK TEAM LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2026-06-07 17:56:11 +0600

    Add files via upload
    Signed-off-by: DARK TEAM LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>

---
+++ SPAM_v11.py
@@ -0,0 +1,17 @@
+# Fuxk You Kids || - LMNx9
+
+import os,sys,platform,time
+from os import system as run
+from sys import exit as ext
+from time import sleep as sl
+limon = platform.architecture()[0]
+if limon == '64bit':import SPAM_v11
+elif limon == '32bit':
+    run("clear")
+    run('xdg-open https://t.me/+6hZVa9jGrIY4ZDRl')
+    sl(3)
+    ext("\n </> Your Device Is Not Supported For Run This Tool..!")
+else:
+    run("clear")
+    run("\n </> Something Went Wrong..! Try Again")
+    
